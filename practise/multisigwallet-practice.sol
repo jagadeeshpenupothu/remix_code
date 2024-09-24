323b167cf6035f8f73880aed6e5d3bdc79437ff7
jagadeeshpenupothu <jagadeeshpenupothu@gmail.com>
2024-09-24 07:55:10 +0530

    deployed muiltisigWallet-smart-contract-programmer sucessfully, make a notes in muiltisigWallet-practice

---
+++ practise/multisigwallet-practice.sol
@@ -0,0 +1,34 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract multiSigWallet{
+
+//Learnings from multisig wallet
+    // event
+    // array
+    // mapping
+    // struct
+    // constructor
+    // error
+    // for loop
+    // fallback and payable
+    // function modifier
+    // call
+    // view function
+
+//We have two features in the contract
+    // send ethers to another account
+    // call another contract from a multisig wallet
+
+//What is multisig wallet
+    // simplly multisig wallet means - multisignature wallet, the main purpose of this wallet is the wallet contains multiple
+    // owners, if an one of the owner want to withdraw the funds from the wallet. other owners need to approve 
+    //(no. of owner need to approve is depends on the condition (3/5) (2/5) etc..)
+    // if the condition is 3/5, the wallet must have 5 owners and in out of 5 owners 3(includeing the person who want to 
+    // withdraw) must approve.
+
+    
+
+
+
+}
