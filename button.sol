c69cd7c853a0151d12654bca4a26c7c214207c56
jagadeeshpenupothu <jagadeeshpenupothu@gmail.com>
2024-09-25 07:59:59 +0530

    done experiment with remix function color buttons

---
+++ button.sol
@@ -0,0 +1,20 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract button{
+    uint public blue1;
+
+    function blue2() pure public {
+        uint total;
+        total = 2+2;
+    }
+
+    function orange() public {
+        blue1 = 2+2;
+    }
+
+    function red() public payable{
+
+    }
+
+}
