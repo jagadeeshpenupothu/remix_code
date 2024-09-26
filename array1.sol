3b0851351cf60d0c3b8dc6ca57c0c6458e171822
jagadeeshpenupothu <jagadeeshpenupothu@gmail.com>
2024-09-26 07:09:19 +0530

    practised array with for loop

---
+++ array1.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract arrayRemoveElementsByShifiting{
+    uint256[] arr = [1,2,3,4,5];
+    function RemoveIndexValue(uint256 _index) public{
+        require(_index < arr.length, "give another");
+        for (uint256 i = _index; i < arr.length-1; i++){
+            arr[i] = arr[i+1];
+        }
+        arr.pop();
+    }
+
+
+}
