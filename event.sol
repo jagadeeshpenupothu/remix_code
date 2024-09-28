cdecc2a58d78ea25042507e8dd421635624de5be
jagadeeshpenupothu <jagadeeshpenupothu@gmail.com>
2024-09-28 06:50:35 +0530

    added comments to events file

--- event.sol
+++ event.sol
@@ -2,7 +2,10 @@
 pragma solidity ^0.8.24;
 
 contract practice_event{
+    //Event definitation
     event log(address indexed account_address, string msg);
+
+    //Event call
     function trigger_log_event() public{
         emit log(msg.sender,"hello");
     }
